d0b65c6cc135e413046a42b540ac51a3cba1fe51
shiitake <pegtar377512@gmail.com>
2024-11-05 05:49:33 +0900

    first commit

---
+++ intro/data.py
@@ -0,0 +1,7 @@
+name = "Yamada Taro"  # 文字列
+age = 20              # 整数
+height = 170.5        # 浮動小数点数
+is_student = True     # 真偽値
+
+# print("私は" + age + "歳です。")    # エラー
+print("私は" + str(age) + "歳です。")    # OK
