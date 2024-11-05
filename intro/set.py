868bb0ec11ffc899ae4ba5d6b817c2a34cf71b9f
shiitake <pegtar377512@gmail.com>
2024-11-05 21:54:00 +0900

    first commit

---
+++ intro/set.py
@@ -0,0 +1,13 @@
+#1.セットの作成
+my_set = {'apple', 'banana', 'cherry'}
+print(f"1.セットの作成: {my_set}")
+
+#2.要素の追加
+my_set.add('orange')
+print(f"2.要素の追加(1回目): {my_set}")
+my_set.add('orange')
+print(f"2.要素の追加(2回目): {my_set}")
+
+#3.要素の削除
+my_set.remove('banana')
+print(f"3.要素の削除: {my_set}")
