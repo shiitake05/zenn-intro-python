09f3bf3e9f869c7d80d21f3ab0edc4666aa742a0
shiitake <pegtar377512@gmail.com>
2024-11-05 21:49:54 +0900

    first commit

---
+++ intro/def.py
@@ -0,0 +1,19 @@
+#関数を定義
+# def greet():
+    # print("こんにちは！")
+
+#関数の呼び出し
+# greet()
+
+#関数を定義
+def greet(name):
+    print(name + "さん、こんにちは！")
+
+#関数の呼び出し
+greet("太郎")
+
+#関数を定義
+def add(a, b):
+    return a + b
+#関数の呼び出し
+print(add(2,3))
