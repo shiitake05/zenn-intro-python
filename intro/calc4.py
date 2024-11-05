a39fc32284bef4ce7e19a1d5ed2740719120d0e9
shiitake <pegtar377512@gmail.com>
2024-11-05 21:49:13 +0900

    first commit

---
+++ intro/calc4.py
@@ -0,0 +1,27 @@
+# 足し算
+addition = 5 + 2
+print("足し算の結果:", addition)
+
+# 引き算
+subtraction = 5 - 2
+print("引き算の結果:", subtraction)
+
+# 掛け算
+multiplication = 5 * 2
+print("掛け算の結果:", multiplication)
+
+# 割り算
+division = 5 / 2
+print("割り算の結果:", division)
+
+# 整数の割り算 (小数点以下切り捨て)
+integer_division = 5 // 2
+print("整数の割り算の結果:", integer_division)
+
+# 剰余 (割り算の余り)
+remainder = 5 % 2
+print("剰余の結果:", remainder)
+
+# 累乗
+exponentiation = 5 ** 2
+print("累乗の結果:", exponentiation)
