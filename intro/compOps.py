6a790abeb7951b694f858454f86eb4d904160037
shiitake <pegtar377512@gmail.com>
2024-11-05 21:49:36 +0900

    first commit

---
+++ intro/compOps.py
@@ -0,0 +1,23 @@
+# 等しい
+is_equal = 5 == 2
+print("5 == 2:", is_equal)  # 出力: False
+
+# 等しくない
+is_not_equal = 5 != 2
+print("5 != 2:", is_not_equal)  # 出力: True
+
+# より大きい
+is_greater = 5 > 2
+print("5 > 2:", is_greater)  # 出力: True
+
+# より小さい
+is_less = 5 < 2
+print("5 < 2:", is_less)  # 出力: False
+
+# 以上
+is_greater_or_equal = 5 >= 2
+print("5 >= 2:", is_greater_or_equal)  # 出力: True
+
+# 以下
+is_less_or_equal = 5 <= 2
+print("5 <= 2:", is_less_or_equal)  # 出力: False
