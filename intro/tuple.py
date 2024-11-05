6437b7eb21bc42d853399b032067973924b3c155
shiitake <pegtar377512@gmail.com>
2024-11-05 21:55:20 +0900

    first commit

---
+++ intro/tuple.py
@@ -0,0 +1,9 @@
+# tupleはリストと似ているが、変更できない
+my_tuple = (1, "Hello", 3.14)
+
+#1.タプルの作成
+my_tuple = ("apple", "banana", "cherry")
+print(f"1.タプルの作成: {my_tuple}")
+
+#2.要素へアクセス
+print(f"2.要素へアクセス: {my_tuple[1]}")
